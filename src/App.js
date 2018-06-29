7f6afac6f41dd3a717a51c7d04e0ba97ce8fabeb
Your Name <acidduckling@gmail.com>
2018-06-29 13:32:41 +1000

    Initial Commit - Basic multi "page" react

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route: 'page1'
+    };
+  }
+  render() {
+    if (this.state.route === 'page3') return <Page3 onRouteChange={this.onRouteChange} />;
+    else if (this.state.route === 'page2') return <Page2 onRouteChange={this.onRouteChange} />;
+
+    return <Page1 onRouteChange={this.onRouteChange} />;
+  }
+
+  onRouteChange = route => {
+    this.setState({ route });
+  };
+}
+
+export default App;
